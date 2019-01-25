5d1f36a0a30dca77452855bd345a66ed62d141ec
LodurrWasTaken <evvmoroz@gmail.com>
2019-01-25 19:27:15 +0200

    init2

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const http = require('http');
 const server = http.createServer();
 const io = require('socket.io')(server);
 
+let players = {};
+
 io.on('connection', socket => {
     console.log(`new conn: ${socket.id}`);
+    players[socket.id] = socket;
+    io.emit('broadcast', 'NEW CONNECTION');
 });
 
 server.listen(9009, () => console.log('Listening on 9009...'));
