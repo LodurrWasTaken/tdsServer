ecd04f4ec09f5962388025e59add64ec22533a50
LodurrWasTaken <evvmoroz@gmail.com>
2019-01-27 15:06:09 +0200

    playerUpdate

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const http = require('http');
 const server = http.createServer();
 const io = require('socket.io')(server);
 
-let players = {};
+let players = [];
 
 io.on('connection', socket => {
     console.log(`new conn: ${socket.id}`);
-    players[socket.id] = socket;
-    io.emit('broadcast', 'NEW CONNECTION');
+    players.push(socket.id);
+
+    // io.emit('onConnection', socket.id);
+    io.emit('onConnection', players);
+
+    socket.on('disconnect', () => {
+        players.splice(players.indexOf(socket.id), 1);    
+        io.emit('onDisconnect', socket.id);
+        console.log('LOST CONNECTION')
+    });
+
+    socket.on('playerUpdate', (data) => {
+        io.emit('playerUpdate', data);
+    });
 });
 
 server.listen(9009, () => console.log('Listening on 9009...'));
