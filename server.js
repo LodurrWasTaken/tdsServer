491430bbf972c423ddaba7471ed5516904eddfee
LodurrWasTaken <evvmoroz@gmail.com>
2019-02-10 14:43:47 +0200

    lobbies

--- server.js
+++ server.js
@@ -2,27 +2,55 @@ const http = require('http');
 const server = http.createServer();
 const io = require('socket.io')(server);
 
-let players = [];
+let rooms = {};
 
 io.on('connection', socket => {
-    console.log(`new conn: ${socket.id}`);
-    players.push(socket.id);
-
-    // io.emit('onConnection', socket.id);
-    io.emit('onConnection', players);
+    socket.emit('onConnection', Object.keys(rooms));
 
     socket.on('disconnect', () => {
-        players.splice(players.indexOf(socket.id), 1);    
-        io.emit('onDisconnect', socket.id);
-        console.log('LOST CONNECTION')
+        if (!socket.room) return;
+
+        rooms[socket.room].splice(rooms[socket.room].indexOf(socket.id), 1);
+        if (!rooms[socket.room].length) {
+            delete rooms[socket.room];
+        }
+        io.in(socket.room).emit('onDisconnect', socket.id);
+        
+        socket.leaveAll();
     });
 
     socket.on('actorUpdate', data => {
-        io.emit('actorUpdate', data);
+        io.in(socket.room).emit('actorUpdate', data);
     });
 
     socket.on('projectileSpawn', data => {
-        io.emit('projectileSpawn', data);
+        io.in(socket.room).emit('projectileSpawn', data);
+    });
+
+    socket.on('createLobby', playerName => {
+        socket.name = playerName;
+        socket.room = socket.id;
+
+        socket.join(socket.id);
+        rooms[socket.id] = [socket];
+        socket.emit('createLobby', { socketId: socket.id, name: playerName });
+        io.emit('newLobby', socket.id);
+    });
+
+    socket.on('joinLobby', data => {
+        socket.name = data.name;
+        socket.room = data.lobbyId;
+
+        io.in(data.lobbyId).emit('newPlayer', { socketId: socket.id, name: data.name });
+        socket.join(data.lobbyId);
+        socket.emit('joinLobby', {
+            playerInfo: {
+                socketId: socket.id,
+                name: data.name,
+            },
+            players: rooms[data.lobbyId].map(player => ({ socketId: player.id, name: player.name }))
+        });
+        rooms[data.lobbyId].push(socket);
     });
 });
 
